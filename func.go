bf63d1f91254a6ea0eb796f9ae26db6890342ea7
zhuxinhao <1838370135@qq.com>
2018-11-19 18:40:06 +0800

    增加四则运算函数

--- func.go
+++ func.go
@@ -1,1 +1,24 @@
 package main
+
+import "fmt"
+
+func myeval(a, b int, op string) int  {
+	switch op {
+	case "+":
+		return a+b
+	case "-":
+		return a-b
+	case "*":
+		return a*b
+	case "/":
+		return a/b
+	default:
+		panic("unsupported operation:" +  op)
+	}
+	
+}
+
+func main()  {
+	fmt.Println(myeval(3,4, "*"))
+
+}
